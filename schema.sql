dcc06472ad5e441813d5b7d9b94ba55aea0171b2
Zach Hilman <me@zachhilman.net>
2021-11-13 21:43:41 -0500

    Finish schema based on whiteboard

---
+++ schema.sql
@@ -0,0 +1,34 @@
+create table project
+(
+    name     varchar(100)   not null primary key,
+    archived boolean not null default false
+);
+
+create table task
+(
+    id      int                                       not null primary key auto_increment,
+    name    varchar(100)                                         not null,
+    parent  int                                          null,
+    status  enum ('complete', 'in_progress', 'subtasks') not null default 'in_progress',
+    project varchar(100)                                         not null,
+    foreign key (parent) references task (id) on delete cascade on update cascade,
+    foreign key (project) references project (name) on delete cascade on update cascade
+);
+
+create table teammate
+(
+    name    varchar(100) not null,
+    project varchar(100) not null,
+    primary key (name, project),
+    foreign key (project) references project (name) on delete cascade on update cascade
+);
+
+create table task_assignments
+(
+    task             int  not null,
+    teammate_name    varchar(100) not null,
+    teammate_project varchar(100) not null,
+    primary key (task, teammate_name, teammate_project),
+    foreign key (task) references task (id) on delete cascade on update cascade,
+    foreign key (teammate_name, teammate_project) references teammate (name, project) on delete cascade on update cascade
+);
