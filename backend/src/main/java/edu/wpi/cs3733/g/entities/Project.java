675e0f36cd6ce10dea749e32b54e5562fd6c753f
Iv <aerobinsoniv@gmail.com>
2021-11-19 13:39:03 -0500

    Add project name and archive methods

--- backend/src/main/java/edu/wpi/cs3733/g/entities/Project.java
+++ backend/src/main/java/edu/wpi/cs3733/g/entities/Project.java
@@ -2,12 +2,21 @@ package edu.wpi.cs3733.g.entities;
 
 public class Project {
     private String _name;
-    
-    public String getName(){
-        return _name;
-    }
+    private boolean _archived = false;
 
     public Project(String name){
         _name = name;
+    }
+    
+    public String getName(){
+        return _name;   
+    }
+
+    public boolean isArchived(){
+        return _archived;
+    }
+
+    public void archive(){
+        _archived = true;
     }
 }
--- backend/src/test/java/edu/wpi/cs3733/g/tests/TestProject.java
+++ backend/src/test/java/edu/wpi/cs3733/g/tests/TestProject.java
@@ -9,9 +9,35 @@ import edu.wpi.cs3733.g.entities.Project;
 public class TestProject {
 
     @Test
-    public void TestName(){
+    public void testName(){
         Project testProject = new Project("Starship");
         assertTrue(testProject.getName().equals("Starship"));
 
+    }
+
+    @Test
+    public void testNameNull(){
+        Project testProject = new Project(null);
+        assertFalse(testProject.getName() != null);
+
+    }
+
+    @Test
+    public void testNameEmpty(){
+        Project testProject = new Project("");
+        assertTrue(testProject.getName().equals(""));
+
+    }
+
+    @Test
+    public void testArchive(){
+        Project testProject = new Project("Starship");
+        assertFalse(testProject.isArchived());
+        testProject.archive();
+        assertTrue(testProject.isArchived());
+
+        //Archive shouldn't be a toggle
+        testProject.archive();  
+        assertTrue(testProject.isArchived());
     }
 }
