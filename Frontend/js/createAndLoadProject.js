4dee115e194c346440cd0393a22abf32f3244bc5
jatinkohli <jatinpkohli@gmail.com>
2021-11-30 21:09:11 -0500

    Combine create/load project and render project into one html file and complete functionality

--- Frontend/js/createAndLoadProject.js
+++ Frontend/js/createAndLoadProject.js
@@ -11,6 +11,13 @@ function handleCreateProjectClick(e) {
     xhr.onloadend = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             console.log(xhr.responseText)
+            var jsonResponse = JSON.parse(xhr.responseText)
+
+            if (jsonResponse["error"] == "Failed to create project.") {
+                alert('Project with name ' + jsonResponse["name"] + ' already exists.')
+            } else {
+                renderProject(jsonResponse["name"])
+            }
         } else {
             alert('Something went wrong')
         }
@@ -30,8 +37,28 @@ function handleLoadProjectClick(e) {
     xhr.onloadend = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             console.log(xhr.responseText)
+            var jsonResponse = JSON.parse(xhr.responseText)
+
+            if (jsonResponse["name"] == "Failed") {
+                alert('Project with name ' + name + ' does not exist.')
+            } else {
+                renderProject(jsonResponse["name"])
+            }
         } else {
             alert('Something went wrong')
         }
     }
+}
+
+function renderProject(name) {
+    console.log("rendering project with name: " + name)
+
+    document.getElementById("create-header").innerHTML = "Project " + name
+
+    document.getElementById("create-project-form").setAttribute("class", "hidden")
+    document.getElementById("load-header").setAttribute("class", "hidden")
+    document.getElementById("load-project-form").setAttribute("class", "hidden")
+
+    document.getElementById("new-task-btn").setAttribute("class", "padded")
+    document.getElementById("task-view").setAttribute("class", "padded")
 }
