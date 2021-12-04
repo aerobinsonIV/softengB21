6052c54e7618bd9fe0e786022aef981b8772f852
Eli Benevedes <benevedeselias@gmail.com>
2021-12-04 16:55:39 -0500

    ArchiveProjectController working

--- backend/src/main/java/edu/wpi/cs3733/g/controllers/ArchiveProjectController.java
+++ backend/src/main/java/edu/wpi/cs3733/g/controllers/ArchiveProjectController.java
@@ -1,4 +1,30 @@
 package edu.wpi.cs3733.g.controllers;
 
-public class ArchiveProjectController {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+import edu.wpi.cs3733.g.entities.Project;
+import edu.wpi.cs3733.g.requests.ArchiveProjectRequest;
+
+public class ArchiveProjectController implements RequestHandler<ArchiveProjectRequest, Project> {
+
+    @Override
+    public Project handleRequest(ArchiveProjectRequest req, Context context) {
+
+        Project project = new Project(req.getProjectName());
+
+        try {
+            boolean success = DatabaseAccess.updateProjectArchived(project, true);
+
+            if(success) {
+                project.archive();
+
+                return project;
+            } else {
+                return new Project("Failed");
+            }
+        } catch (Exception e) {
+            return new Project("Failed");
+        }
+    }
 }
---
+++ backend/src/main/java/edu/wpi/cs3733/g/responses/ArchiveProjectResponse.java
@@ -0,0 +1,4 @@
+package edu.wpi.cs3733.g.responses;
+
+public class ArchiveProjectResponse {
+}
