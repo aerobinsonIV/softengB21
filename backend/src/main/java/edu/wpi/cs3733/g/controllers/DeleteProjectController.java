2b98aefb6ad436d6949fc805d8901a9db2320f1b
Zach Hilman <me@zachhilman.net>
2021-12-04 19:48:31 -0500

    Implement delete project controller

--- backend/src/main/java/edu/wpi/cs3733/g/controllers/DeleteProjectController.java
+++ backend/src/main/java/edu/wpi/cs3733/g/controllers/DeleteProjectController.java
@@ -1,4 +1,38 @@
 package edu.wpi.cs3733.g.controllers;
 
-public class DeleteProjectController {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+import edu.wpi.cs3733.g.entities.Project;
+
+class DeleteProjectData {
+    String name;
+
+    public DeleteProjectData() {
+    }
+
+    public DeleteProjectData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class DeleteProjectController implements RequestHandler<DeleteProjectData, DeleteProjectData> {
+    @Override
+    public DeleteProjectData handleRequest(DeleteProjectData input, Context context) {
+        try {
+            if (DatabaseAccess.deleteProject(new Project(input.name))) {
+                return input;
+            }
+        } catch (Exception ignored) {
+        }
+        return null;
+    }
 }
