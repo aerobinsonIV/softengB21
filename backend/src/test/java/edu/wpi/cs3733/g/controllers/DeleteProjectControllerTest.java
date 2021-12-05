c8e814727e0c04eeb57facb4e74331a7e3357c2a
Zach Hilman <me@zachhilman.net>
2021-12-04 19:50:36 -0500

    Add unit test for delete project controller

---
+++ backend/src/test/java/edu/wpi/cs3733/g/controllers/DeleteProjectControllerTest.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs3733.g.controllers;
+
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+import edu.wpi.cs3733.g.entities.Project;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DeleteProjectControllerTest extends BaseControllerTest {
+    @Test
+    void test() throws Exception {
+        DeleteProjectController controller = new DeleteProjectController();
+
+        Project p1 = new Project("P1");
+        Project p2 = new Project("P2");
+
+        DatabaseAccess.createProject(p1);
+        DatabaseAccess.createProject(p2);
+
+        Assertions.assertNotNull(controller.handleRequest(new DeleteProjectData(p1.getName()), null));
+        Assertions.assertNotNull(controller.handleRequest(new DeleteProjectData(p2.getName()), null));
+    }
+}
