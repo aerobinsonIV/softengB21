ecc9dafc7a34125774f1834f5d77dd54ff7a5165
Zach Hilman <me@zachhilman.net>
2021-12-04 19:48:38 -0500

    Implement remove teammate controller

--- backend/src/main/java/edu/wpi/cs3733/g/controllers/RemoveTeammateController.java
+++ backend/src/main/java/edu/wpi/cs3733/g/controllers/RemoveTeammateController.java
@@ -1,4 +1,21 @@
 package edu.wpi.cs3733.g.controllers;
 
-public class RemoveTeammateController {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+import edu.wpi.cs3733.g.entities.Project;
+import edu.wpi.cs3733.g.entities.Teammate;
+
+public class RemoveTeammateController implements RequestHandler<Teammate, Project> {
+    @Override
+    public Project handleRequest(Teammate input, Context context) {
+        try {
+            Project project = new Project(input.getProject());
+            if (DatabaseAccess.removeTeammate(input)) {
+                return DatabaseAccess.getProject(project);
+            }
+        } catch (Exception ignored) {
+        }
+        return null;
+    }
 }
