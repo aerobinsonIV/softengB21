cd246195050a662b4405a313718c7e8b791728fc
Zach Hilman <me@zachhilman.net>
2021-12-04 19:49:47 -0500

    Add absolute setArchived function to Project

--- backend/src/main/java/edu/wpi/cs3733/g/entities/Project.java
+++ backend/src/main/java/edu/wpi/cs3733/g/entities/Project.java
@@ -8,71 +8,71 @@ public class Project {
     private ArrayList<Task> _tasks = new ArrayList<>();
     private ArrayList<Teammate> _team = new ArrayList<>();
 
-    public Project(String name){
+    public Project(String name) {
         _name = name;
     }
-    
-    public String getName(){
-        return _name;   
+
+    public String getName() {
+        return _name;
     }
 
-    public boolean getIsArchived(){
+    public boolean getIsArchived() {
         return _isArchived;
     }
 
-    public void archive(){
-        _isArchived = true;
+    public void setArchived(boolean value) {
+        _isArchived = value;
     }
 
-    public void addTeammate(Teammate t){
+    public void addTeammate(Teammate t) {
         _team.add(t);
     }
 
-    public void removeTeammate(Teammate t){
+    public void removeTeammate(Teammate t) {
         _team.remove(t);
     }
 
-    public void removeTeammate(String name){
+    public void removeTeammate(String name) {
         //Loop through all teammates
-        for(Teammate curTeammate : _team){
+        for (Teammate curTeammate : _team) {
 
             //If we find a teammate whose name matches, remove
-            if(curTeammate.getName() == name){
+            if (curTeammate.getName() == name) {
                 _team.remove(curTeammate);
                 return;
             }
         }
     }
 
-    public ArrayList<Teammate> getTeam(){
+    public ArrayList<Teammate> getTeam() {
         return _team;
     }
 
-    public void addTask(Task t){
+    public void addTask(Task t) {
         _tasks.add(t);
     }
 
-    public void removeTask(Task t){
+    public void removeTask(Task t) {
         _tasks.remove(t);
     }
 
-    public void removeTask(int id){
+    public void removeTask(int id) {
         _tasks.remove(getTask(id));
     }
 
-    public Task getTask(int id){
+    public Task getTask(int id) {
         //Loop through all assigned tasks
-        for(Task curTask : _tasks){
+        for (Task curTask : _tasks) {
 
             //If we find a task whose id matches, return
-            if(curTask.getId() == id){
+            if (curTask.getId() == id) {
                 return curTask;
             }
         }
         return null;
     }
 
-    public ArrayList<Task> getTasks(){
+    public ArrayList<Task> getTasks() {
         return _tasks;
     }
 }
