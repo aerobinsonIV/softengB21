c8376165ec1ddc8a5734907e500973bc8f45712b
Zach Hilman <me@zachhilman.net>
2021-12-04 19:50:31 -0500

    Add unit test for create teammate controller

---
+++ backend/src/test/java/edu/wpi/cs3733/g/controllers/CreateTeammateControllerTest.java
@@ -0,0 +1,25 @@
+package edu.wpi.cs3733.g.controllers;
+
+import edu.wpi.cs3733.g.entities.Project;
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+import edu.wpi.cs3733.g.entities.Teammate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CreateTeammateControllerTest extends BaseControllerTest {
+    @Test
+    void test() throws Exception {
+        DatabaseAccess.createProject(new Project("P1"));
+        DatabaseAccess.createProject(new Project("P2"));
+
+        Teammate t1 = new Teammate("A", "P1");
+        Teammate t2 = new Teammate("B", "P1");
+        Teammate t3 = new Teammate("A", "P2");
+
+        CreateTeammateController controller = new CreateTeammateController();
+
+        Assertions.assertNotNull(controller.handleRequest(t1, null));
+        Assertions.assertNotNull(controller.handleRequest(t2, null));
+        Assertions.assertNotNull(controller.handleRequest(t3, null));
+    }
+}
