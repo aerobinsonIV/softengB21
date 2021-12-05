d096f48ce4cdd4f7e0cf9e5222ce22b914b6b72e
Zach Hilman <me@zachhilman.net>
2021-12-04 19:48:23 -0500

    Implement create teammate controller

--- backend/src/main/java/edu/wpi/cs3733/g/controllers/CreateTeammateController.java
+++ backend/src/main/java/edu/wpi/cs3733/g/controllers/CreateTeammateController.java
@@ -1,4 +1,36 @@
 package edu.wpi.cs3733.g.controllers;
 
-public class CreateTeammateController {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import edu.wpi.cs3733.g.entities.Teammate;
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+
+class CreateTeammateResponse {
+    String name;
+
+    public CreateTeammateResponse() {}
+
+    public CreateTeammateResponse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class CreateTeammateController implements RequestHandler<Teammate, CreateTeammateResponse> {
+    @Override
+    public CreateTeammateResponse handleRequest(Teammate input, Context context) {
+        try {
+            if (DatabaseAccess.createTeammate(input)) {
+                return new CreateTeammateResponse(input.getName());
+            }
+        } catch (Exception ignored) {}
+        return null;
+    }
 }
