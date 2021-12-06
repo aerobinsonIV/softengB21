962582a126531414980e9f2f50cc96db38fc899f
jatinkohli <jatinpkohli@gmail.com>
2021-12-06 00:03:34 -0500

    Add html (and fix js) for g3 iteration 2 deliverable (add/remove teammate and add task)

--- frontend/js/participant.js
+++ frontend/js/participant.js
@@ -1,26 +1,30 @@
 function handleCreateProjectClick(e) {
     var name = document.getElementById("create-input").value
 
-    var url = 'https://5odsqadon5.execute-api.us-east-1.amazonaws.com/Alpha/project/'
+    if (name == '') {
+        alert('Please input a name into the box below \'Create Project\'')
+    } else {
+        var url = 'https://5odsqadon5.execute-api.us-east-1.amazonaws.com/Alpha/project/'
 
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', url + name, true)
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', url + name, true)
 
-    xhr.send()
+        xhr.send()
 
-    xhr.onloadend = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.responseText)
-            var jsonResponse = JSON.parse(xhr.responseText)
+        xhr.onloadend = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                console.log(xhr.responseText)
+                var jsonResponse = JSON.parse(xhr.responseText)
 
-            if (jsonResponse["error"] == "Failed to create project.") {
-                alert('Project with name ' + jsonResponse["name"] + ' already exists.')
+                if (jsonResponse["error"] == "Failed to create project.") {
+                    alert('Project with name ' + jsonResponse["name"] + ' already exists.')
+                } else {
+                    jsonResponse["isArchived"] = false // simulate actually returning a project object with an isArchived value
+                    renderProject(jsonResponse)
+                }
             } else {
-                jsonResponse["isArchived"] = false // simulate actually returning a project object with an isArchived value
-                renderProject(jsonResponse)
+                alert('Something went wrong')
             }
-        } else {
-            alert('Something went wrong')
         }
     }
 }
@@ -28,25 +32,29 @@ function handleCreateProjectClick(e) {
 function handleLoadProjectClick(e) {
     var name = document.getElementById("load-input").value
 
-    var url = 'https://5odsqadon5.execute-api.us-east-1.amazonaws.com/Alpha/project/'
+    if (name == '') {
+        alert('Please input a name into the box below \'Load Project\'')
+    } else {
+        var url = 'https://5odsqadon5.execute-api.us-east-1.amazonaws.com/Alpha/project/'
 
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', url + name, true)
+        var xhr = new XMLHttpRequest()
+        xhr.open('GET', url + name, true)
 
-    xhr.send()
+        xhr.send()
 
-    xhr.onloadend = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.responseText)
-            var jsonResponse = JSON.parse(xhr.responseText)
+        xhr.onloadend = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                console.log(xhr.responseText)
+                var jsonResponse = JSON.parse(xhr.responseText)
 
-            if (jsonResponse["name"] == "Failed") {
-                alert('Project with name ' + name + ' does not exist.')
+                if (jsonResponse["name"] == "Failed") {
+                    alert('Project with name ' + name + ' does not exist.')
+                } else {
+                    renderProject(jsonResponse)
+                }
             } else {
-                renderProject(jsonResponse)
+                alert('Something went wrong')
             }
-        } else {
-            alert('Something went wrong')
         }
     }
 }
@@ -60,12 +68,14 @@ function renderProject(response) {
     document.getElementById("create-header").innerHTML = "Project " + name
     document.getElementById("load-header").innerHTML = (isArchived ? "" : "Not ") + "Archived"
 
+    var hiddenElements = document.getElementsByClassName("hidden")
+    var len = hiddenElements.length
+    for (var index = 0; index < len; len--) {
+        hiddenElements[index].setAttribute("class", "shown") // hiddenElements shrinks by one element every time this is called
+    }
+
     document.getElementById("create-project-form").setAttribute("class", "hidden")
     document.getElementById("load-project-form").setAttribute("class", "hidden")
-
-    document.getElementById("landing-btn").setAttribute("class", "padded")
-    document.getElementById("new-task-btn").setAttribute("class", "padded")
-    document.getElementById("task-view").setAttribute("class", "padded")
 }
 
 function renderLandingPage() {
@@ -75,7 +85,12 @@ function renderLandingPage() {
     document.getElementById("create-project-form").setAttribute("class", "")
     document.getElementById("load-project-form").setAttribute("class", "")
 
+    var shownElements = document.getElementsByClassName("shown")
+    var len = shownElements.length
+    for (var index = 0; index < len; len--) {
+        shownElements[index].setAttribute("class", "hidden") // hiddenElements shrinks by one element every time this is called
+    }
+
     document.getElementById("landing-btn").setAttribute("class", "hidden")
-    document.getElementById("new-task-btn").setAttribute("class", "hidden")
     document.getElementById("task-view").setAttribute("class", "hidden")
 }
