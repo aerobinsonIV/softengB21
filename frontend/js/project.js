8ff23be8528981808d88a554efe39ee890b20117
jatinkohli <jatinpkohli@gmail.com>
2021-12-06 08:17:11 -0500

    Modify frontend for adding teammates

--- frontend/js/project.js
+++ frontend/js/project.js
@@ -30,3 +30,33 @@ function addTask() {
     }
 }
 
+function addTeammate() {
+    var projectHeader = document.getElementById("create-header").innerHTML
+    var projectName = projectHeader.substr(projectHeader.indexOf(" ") + 1)
+    var teammateName = document.getElementById("add-teammate-input").value
+
+    if (teammateName == '') {
+        alert('Please input a name into the box below \'Add Teammate\'')
+    } else {
+        var url = 'https://5odsqadon5.execute-api.us-east-1.amazonaws.com/G3/teammate/' + projectName + "/"
+
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', url + teammateName, true)
+
+        xhr.send()
+
+        xhr.onloadend = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                console.log(xhr.responseText)
+
+                if (xhr.responseText == null || xhr.responseText == "null") {
+                    alert('Teammate with name ' + teammateName + ' already exists.')
+                } else {
+                    loadProjectWithName(projectName)
+                }
+            } else {
+                alert('Something went wrong')
+            }
+        }
+    }
+}
