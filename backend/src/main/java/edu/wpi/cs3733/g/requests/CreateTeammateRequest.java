042b8c63faf36b98da8d128cb70204a4d94ad1ac
Iv <aerobinsoniv@gmail.com>
2021-12-06 03:21:31 -0500

    Started API paths for create and remove teammates

---
+++ backend/src/main/java/edu/wpi/cs3733/g/requests/CreateTeammateRequest.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs3733.g.requests;
+
+public class CreateTeammateRequest {
+    String name;
+    String projectName;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setProjectName(String projectName){
+        this.projectName = projectName;
+    }
+
+    public String getProjectName(){
+        return projectName;
+    }
+}
---
+++ backend/src/main/java/edu/wpi/cs3733/g/requests/RemoveTeammateRequest.java
@@ -0,0 +1,27 @@
+package edu.wpi.cs3733.g.requests;
+
+public class RemoveTeammateRequest {
+    String name;
+    String projectName;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setProjectName(String projectName){
+        this.projectName = projectName;
+    }
+
+    public String getProjectName(){
+        return projectName;
+    }
+
+    public RemoveTeammateRequest(String name, String projectName){
+        this.name = name;
+        this.projectName = projectName;
+    }
+}
