dd84ce707e1250fa0411ccce2510124a98c14355
Iv <aerobinsoniv@gmail.com>
2021-12-09 20:28:08 -0500

    Create skeleton for AssignTask request and controller

--- backend/src/main/java/edu/wpi/cs3733/g/controllers/AssignTaskController.java
+++ backend/src/main/java/edu/wpi/cs3733/g/controllers/AssignTaskController.java
@@ -1,4 +1,14 @@
 package edu.wpi.cs3733.g.controllers;
 
-public class AssignTaskController {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import edu.wpi.cs3733.g.entities.Task;
+import edu.wpi.cs3733.g.requests.AssignTaskRequest;
+
+public class AssignTaskController implements RequestHandler<AssignTaskRequest, Task> {
+    @Override
+    public Task handleRequest(AssignTaskRequest req, Context context) {
+        return null;
+    }
 }
---
+++ backend/src/main/java/edu/wpi/cs3733/g/requests/AssignTaskRequest.java
@@ -0,0 +1,5 @@
+package edu.wpi.cs3733.g.requests;
+
+public class AssignTaskRequest {
+    
+}
