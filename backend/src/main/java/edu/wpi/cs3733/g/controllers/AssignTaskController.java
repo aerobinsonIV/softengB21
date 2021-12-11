337e22b592f7dc61c94c8412c6af4837f37baff5
Iv <aerobinsoniv@gmail.com>
2021-12-11 00:50:47 -0500

    AssignTask working

--- backend/src/main/java/edu/wpi/cs3733/g/controllers/AssignTaskController.java
+++ backend/src/main/java/edu/wpi/cs3733/g/controllers/AssignTaskController.java
@@ -1,18 +1,57 @@
 package edu.wpi.cs3733.g.controllers;
 
+import java.util.ArrayList;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import edu.wpi.cs3733.g.db.DatabaseAccess;
+import edu.wpi.cs3733.g.entities.Project;
 import edu.wpi.cs3733.g.entities.Task;
 import edu.wpi.cs3733.g.requests.AssignTaskRequest;
 
 public class AssignTaskController implements RequestHandler<AssignTaskRequest, Task> {
     @Override
     public Task handleRequest(AssignTaskRequest req, Context context) {
-        System.out.println("handleRequest in AssignTaskController called");
-        System.out.println("projectName is " + req.getProjectName());
-        System.out.println("taskId is " + req.getTaskId());
-        System.out.println("teammateName is " + req.getTeammateName());
-        return null;
+        try {
+            if(! DatabaseAccess.checkProjectExists(req.getProjectName())){ // this is where the problem happens
+                System.out.println("Project does not exist");
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to access database to check if project exists");
+            e.printStackTrace();
+            return null;
+        }
+
+        try {
+            DatabaseAccess.createTaskAssignment(req.getProjectName(), req.getTaskId(), req.getTeammateName());
+
+        } catch (Exception e) {
+            System.out.println("Failed create task assignment");
+            e.printStackTrace();
+            return null;
+        }
+
+        try{
+            //Get project that contains this task, then isolate the task and return it
+            Project modifiedProject = DatabaseAccess.getProject(req.getProjectName());
+
+            ArrayList<Task> tasks = modifiedProject.getTasks();
+
+            for(Task task : tasks){
+                if(task.getId() == req.getTaskId()){
+                    return task;
+                }
+            }
+
+            System.out.println("Couldn't find modified task!");
+            return null;
+        }catch (Exception e) {
+            System.out.println("Failed to retrieve modified task");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
